692015527f44fc144939eb116215dcb00c5b5e90
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-08 18:53:17 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,34 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
+  puts sum
+  sum
 end
 
 def subtraction(num1, num2)
-
+  sum = num1 - num2
+  puts sum
+  sum
 end
 
 def division(num1, num2)
-
+  sum = num1 / num2
+  puts sum
+  sum
 end
 
 def multiplication(num1, num2)
-
+  sum = num1 * num2
+  puts sum
+  sum
 end
 
 def modulo(num1, num2)
-
+  sum = num1 % num2
+  puts sum
+  sum
 end
 
 def square_root(num)
-
+  puts Math.sqrt(num)
+  Math.sqrt(num)
 end
